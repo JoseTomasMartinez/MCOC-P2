f472175a0dea90507e7099f9d4f9d07641cccd6e
JoseTomasMartinez <53920966+JoseTomasMartinez@users.noreply.github.com>
2020-10-01 22:27:22 -0300

    Actualización para Entrega 2

--- reticulado.py
+++ reticulado.py
@@ -27,14 +27,49 @@ class Reticulado(object):
         return W
     def obtener_nodos(self):
         return self.xyz
-    def obtener_barras(self):
-    	return self.barras
     def agregar_restriccion(self, nodo, gdl, valor=0.0):
+        if nodo not in self.restricciones:
+            self.restricciones[nodo] = [[gdl, valor]]
+        else:
+            self.restricciones[nodo].append([gdl, valor])
         return
+
     def agregar_fuerza(self, nodo, gdl, valor):
+        if nodo not in self.cargas:
+            self.cargas[nodo] = [[gdl, valor]]
+        else:
+            self.cargas[nodo].append([gdl, valor])
         return
+
     def ensamblar_sistema(self):
-        return
+        """Ensambla el sistema de ecuaciones"""
+        
+        Ngdl = self.Nnodos * self.Ndimensiones
+
+        self.K = np.zeros((Ngdl,Ngdl), dtype=np.double)
+        self.f = np.zeros((Ngdl), dtype=np.double)
+        self.u = np.zeros((Ngdl), dtype=np.double)
+
+        #Iterar sobre las barras
+        for b in self.barras:
+            ke = b.obtener_rigidez(self)
+            fe = b.obtener_vector_de_cargas(self)
+
+            #2D
+            ni = b.obtener_conectividad()[0]
+            nj = b.obtener_conectividad()[1]
+            d = [2*ni, 2*ni+1, 2*nj, 2*nj+1]  #vector d
+            Ndof_per_node = self.Ndimensiones*2
+
+            #MDR
+            for i in range(Ndof_per_node):
+                p = d[i]
+                for j in range(Ndof_per_node):
+                    q = d[j]
+                    self.K[p,q] += ke[i,j]
+                self.f[p] += fe[j]
+
+        return self.K, self.f
     def resolver_sistema(self):
         return
     def recuperar_fuerzas(self):
